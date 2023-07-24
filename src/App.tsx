e878c56f157645f2b61b016af8f1d57eb304ff3a
biel.lil <78098857+biellil@users.noreply.github.com>
2023-07-24 01:16:57 -0300

    24/07
    Nota de atualização: 24/07 - Hoje terminei a estruturação do Projeto TodoTist.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-
+import styles from'./App.module.scss'
+import clipboard from './assets/Clipboard.svg'
 
 export function App() {
  
 
   return (
-  <div></div>
+    <main >
+    
+      <header className={styles.main}> 
+     <div> Tarefas criadas <samp>0</samp></div>
+
+      <div>Concluídas <samp>0</samp></div>
+
+        
+         </header>
+
+         <article className={styles.register}>
+          <img src={clipboard} alt="" />
+         Você ainda não tem tarefas cadastradas
+        <br /> 
+        <samp>Crie tarefas e organize seus itens a fazer</samp>
+         </article>
+    </main>
+
   )
 }
 
